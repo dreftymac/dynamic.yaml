748a104112b2505c0d3d6b0233449dc3aaf478e2
smosley <smosley@uoregon.edu>
2016-03-31 20:50:25 -0700

    local update ;; try set os.chmod

--- py/DDYAML/JinjaFilterDynamicYAML.py
+++ py/DDYAML/JinjaFilterDynamicYAML.py
@@ -381,10 +381,21 @@ if('python_region'):
         ##enddef
 
         ## TODO ;; formalize this function and docs
+        def jjos_hostname(self,jjinput):
+          vout = platform.system()
+          return vout
+        ##enddef
+        ## TODO ;; formalize this function and docs
         def jjos_platform(self,jjinput):
           vout = platform.system()
           return vout
         ##enddef
+        
+        ## import socket
+        ## print(socket.gethostname())
+        ## alias_definition
+        def jjos_hostname(self,jjinput): return self.jjos_platform(jjinput)
+        ##enddef        
 
         def jjscalarize(self,jjinput,myvar=None):
           '''
