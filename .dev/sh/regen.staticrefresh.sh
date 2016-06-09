d564dd71f6e29ff8111e5a6ec6e156a8a2aef665
dreftymac <dreftymac@gmail.com>
2016-06-09 10:56:03 -0700

    local update ;;

---
+++ .dev/sh/regen.staticrefresh.sh
@@ -0,0 +1,9 @@
+### seealso
+### href="smartpath://mymedia/2014/git/github/myclip/myclip.snippets/git.yaml" find="visitant_overload_cults"
+echo "### ********************"
+echo "### running staticrefresh"
+echo ""
+
+echo "### ********************"
+echo "This script has no content yet."
+echo ""
